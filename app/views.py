7c2dfa78d76a88d9f57d7b822249b24bddb2d89e
MridulSelvakumar <mridul07adih@gmail.com>
2024-02-29 10:03:33 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,9 +4,28 @@ from django.shortcuts import render
 
 from app.form import *
 
+from django.http import HttpResponse
 
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
-    return render(request,'registration.html',d)
+
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            
+            return HttpResponse('Registartion Is Successfull')
+        else:
+            return HttpResponse('Invalid Data')
+
+    return render(request,'register.html',d)
